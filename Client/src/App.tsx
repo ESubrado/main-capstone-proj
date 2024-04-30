d9506559c79e939b2b08b83f6417970fde321bd6
Eugene Jr Subrado <eug.subradojr@gmail.com>
2024-04-30 21:48:49 +0800

    Added tsx type components

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css';
+import Navbar from './components/Navbar';
+import Routing from './components/Routing';
+import Footer from './components/Footer';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './App.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <span>This is my project</span>
-    </div>
+    <>
+      <Navbar/>
+      <Routing />
+      <Footer />
+  </>
   );
 }
 
